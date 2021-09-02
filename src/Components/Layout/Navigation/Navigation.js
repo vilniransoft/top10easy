61374b1e134463c30b4db4c39a6e652014c64a0b
thegminero <gminero@pfdrive.com>
2021-09-01 23:27:01 -0400

    fixed mobile alignment

--- src/Components/Layout/Navigation/Navigation.js
+++ src/Components/Layout/Navigation/Navigation.js
@@ -10,13 +10,13 @@ return  <section className="w-full px-8 text-gray-700 bg-white">
                         <img src={logo} alt="Top10easy"/>
                     </span>
                 </Link>
-                <nav className="flex flex-wrap w-full self-center justify-end mb-5 text-base md:mb-0 md:pl-8 md:ml-8 md:border-l md:border-gray-200">
+                <nav className="flex flex-wrap w-full self-center justify-center lg:justify-end mb-5 text-base md:mb-0 md:pl-8 md:ml-8 md:border-l md:border-gray-200">
                     <Link to="/" className="mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900">Home</Link>
                     <Link to="/about" className="mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900">About Us</Link>
                     <Link to="/contact" className="mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900">Contact Us</Link>
                 </nav>
-                <div className="inline-flex items-center ml-5 space-x-6 lg:justify-end">
-                    <Link to="/contact" className="bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded">
+                <div className="inline-flex items-center ml-5 space-x-6 justify-center lg:justify-end">
+                    <Link to="/contact" className="bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent flex-nowrap">
                         Submit your business
                     </Link>
                 </div>
--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -7,13 +7,11 @@ export default function Home(){
     
             <div className="container max-w-lg py-32 mx-auto text-left md:max-w-none md:text-center">
                 <h1 className="text-5xl p-8 font-extrabold leading-10 tracking-tight text-left text-gray-900 md:text-center sm:leading-none md:text-6xl lg:text-7xl"><span className="inline md:block">What Service</span> <span className="relative mt-2 text-transparent bg-clip-text bg-gradient-to-br from-green-500 to-green-400 md:inline-block pb-4">Are you looking for</span></h1>
-                {/* <div className="mx-auto mt-5 text-gray-500 md:mt-12 md:max-w-lg md:text-center lg:text-lg">Simplifying the creation of landing pages, blog pages, application pages and so much more!</div> */}
                 <div className="flex flex-col items-center mt-12 text-center">
-                    <span className="md:relative md:inline-flex w-full md:w-auto items-center">
+                    <div className="flex flex-col-reverse md:flex-row items-center">
                         <Dropdown></Dropdown>
                         <Search></Search>
-                    </span>
-                    
+                    </div>
                 </div>
             </div>
     
