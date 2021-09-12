cc1973cfe117dd610871ab07be15f19de1bedb42
thegminero <gminero@pfdrive.com>
2021-09-11 22:59:54 -0400

    added local install modal

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 'use strict'
 require('dotenv').config();
-const fastify = require('fastify')({ 
+const fastify = require('fastify')({
     logger: true,
-    ignoreTrailingSlash: true ,
+    ignoreTrailingSlash: true,
     wildcard: false
 });
 const path = require('path');
@@ -13,19 +13,23 @@ fastify.register(require('fastify-static'), {
     root: path.join(__dirname, 'build'),
     list: true
 })
-fastify.setNotFoundHandler((req, res) => {
-    res.sendFile('index.html')
-  })
 
+fastify.get("/service-worker.js", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "service-worker.js"));
+});
 fastify.get('/envars', (request, reply) => {
     console.log(process.env.SEARCH_INDEX)
     console.log(process.env.SEARCH_API_KEY)
     const envars = {
-        index:process.env.SEARCH_INDEX,
+        index: process.env.SEARCH_INDEX,
         searchApiKey: process.env.SEARCH_API_KEY
     }
-reply.send(envars)
+    reply.send(envars)
 })
+fastify.setNotFoundHandler((req, res) => {
+    res.sendFile('index.html')
+})
+
 fastify.listen(port, "127.0.0.1", (err, address) => {
     if (err) throw err
     fastify.log.info(`server listening on ${address}`)
