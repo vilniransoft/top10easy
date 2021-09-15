c1e8d2001b4068f93f3352b2a498955f3456b593
thegminero <gminero@pfdrive.com>
2021-09-15 10:54:11 -0400

    fixed margin issue with search input form

--- src/Components/Algolia/AlgoliaSearchBox/AlgoliaSearchBox.js
+++ src/Components/Algolia/AlgoliaSearchBox/AlgoliaSearchBox.js
@@ -8,11 +8,11 @@ export default function AlgoliaSearchBox(){
         const instaSearch = document.querySelector('.ais-SearchBox-input')
         const instaSearchBtn = document.querySelector('.ais-SearchBox-submit')
         const instaCancelSearch = document.querySelector('form > button.ais-SearchBox-reset')
-        instaSearchForm.classList.add('flex', 'w-full', 'ml-8', 'sm:ml-64')
+        instaSearchForm.classList.add('flex', 'w-full', 'ml-0', 'sm:ml-28')
         instaCancelSearch.classList.add("hidden")
         instaSearchBtn.classList.add("bg-green-400", "h-10", "px-5", "pr-6", "rounded-r-lg", "border-1", "border-gray-300")
         instaSearch.classList.add("searchbox", "p-0", "border-1", "border-gray-300", "bg-white", "h-10", "px-5", "rounded-l-lg", 
-        "text-sm", "focus:outline-none", "w-3/5", "px-0")
+        "text-sm", "focus:outline-none", "w-full", "px-0")
     },[])
     return <div className="pt-2 relative mx-auto text-gray-600 w-full">
                 <SearchBox className="" translations={{ placeholder: '',}} />
