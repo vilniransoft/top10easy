0096069cde8e548f0f80db03ed5e698309c68adf
thegminero <gminero@pfdrive.com>
2022-01-17 14:10:03 -0500

    added search state cleanup

--- src/Components/Algolia/AlgoliaSearchBox/AlgoliaSearchBox.js
+++ src/Components/Algolia/AlgoliaSearchBox/AlgoliaSearchBox.js
@@ -17,6 +17,9 @@ export default function AlgoliaSearchBox(){
         instaSearchBtn.classList.add("bg-green-400", "h-10", "px-5", "pr-6", "rounded-r-lg", "border-1", "border-gray-300")
         instaSearch.classList.add("searchbox", "p-0", "border-1", "border-gray-300", "bg-white", "h-10", "px-5", "rounded-l-lg", 
         "text-sm", "focus:outline-none", "w-full", "px-0")
+        return () => {
+            setQuerystate({query: '',refinementList: {}})
+        }
     },[])
     return <div className="pt-2 relative mx-auto text-gray-600 w-full">
                 <SearchBox className="" translations={{ placeholder: '',}} 
