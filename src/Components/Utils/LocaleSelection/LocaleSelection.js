9aee0852d340e041463c5702b9e17b19b66cf987
thegminero <gminero@pfdrive.com>
2022-02-01 22:56:38 -0500

    changed locale selection to fixed top right

--- src/Components/Utils/LocaleSelection/LocaleSelection.js
+++ src/Components/Utils/LocaleSelection/LocaleSelection.js
@@ -30,9 +30,9 @@ export default function LocaleSelection(){
      },[selectedLocale])
 
      return (
-        <div className="w-18 top-16">
+        <div className="right-0 top-0 absolute">
           <Listbox value={selectedLocale} onChange={setSelectedLocale}>
-            <div className="relative mt-1">
+            <div className="relative">
               <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
                 <span className="block truncate">{selectedLocale?.name}</span>
                 <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
