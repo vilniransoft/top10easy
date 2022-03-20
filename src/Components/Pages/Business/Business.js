6f5773d0eee699223520feb06dfccb092cb6d57a
thegminero <gminero@pfdrive.com>
2022-03-20 15:50:03 -0400

    removed no-cors to fetch

--- src/Components/Pages/Business/Business.js
+++ src/Components/Pages/Business/Business.js
@@ -28,9 +28,7 @@ export default function Business(){
         async function getBusiness(){
             // the url is temporary need to change once certs have been properly configured
             const url = `https://top10cms.link/api/v2/pages/?fields=*&type=businesses.BusinessesPage&slug=${params?.name}&locale=${currentLocale}`
-            const serverRes = await fetch(url, {
-                mode: 'no-cors'
-              });
+            const serverRes = await fetch(url);
             const business = await serverRes.json()
             console.log(business)
             if(Object.values(business?.businesses_businessespage[0].businesses_businesshoursorderables).length > 0){
