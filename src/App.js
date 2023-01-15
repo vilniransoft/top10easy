0ef60b790a7dcca8704f7691af15dd9bb78bf6bb
thegminero <75403975+thegminero@users.noreply.github.com>
2023-01-15 11:05:40 -0500

    commented out banner code

--- src/App.js
+++ src/App.js
@@ -58,8 +58,8 @@ function App() {
         <Navigation></Navigation>
       </header>
       <div className="main">
-      { pageLocation.pathname === '/' ? <div style={{backgroundImage: `url("${stateLocation?.cityImgUrl ?? 'https://top10ezdevbucket221148-dev.s3.amazonaws.com/original_images/O2.jpg'}")`}} className="city-image-banner bg-gray-400 w-full h-72 bg-no-repeat bg-cover">
-      </div> : null }
+      {/* { pageLocation.pathname === '/' ? <div style={{backgroundImage: `url("${stateLocation?.cityImgUrl ?? 'https://top10ezdevbucket221148-dev.s3.amazonaws.com/original_images/O2.jpg'}")`}} className="city-image-banner bg-gray-400 w-full h-72 bg-no-repeat bg-cover">
+      </div> : null } */}
       <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/about" element={<About/>} />
